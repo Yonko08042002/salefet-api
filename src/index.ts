975a3a3686ae8f54bc751f8002e51a901e95e06a
Yonko08042002 <tin08042002@gmail.com>
2023-07-21 09:41:15 +0700

    add README

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ const port = process.env.PORT;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Express + TypeScript Server⚡️");
 });
+app.get("/api", (req: Request, res: Response) => {
+  res.send("Hello Express + API TypeScript Server⚡️");
+});
+
+app.get("/api/:id", (req: Request, res: Response) => {
+  res.send("Hello Express + API TypeScript Server⚡️" + req.params.id);
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
